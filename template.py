94226b06a260f7a4fa04a41f56bd1c12001d971e
Adam McHugh <adammchugh83@gmail.com>
2022-04-05 22:27:53 +0930

    Updated template

--- template.py
+++ template.py
@@ -4,34 +4,41 @@ import time
 
 from cortexutils.analyzer import Analyzer
 
+
 class Template(Analyzer):
 
-  def __init__(self):
-    Analyzer.__init__(self)
+    def __init__(self):
+        Analyzer.__init__(self)
 
-  def artifacts(self, raw):
-    return [self.build_artifact(self.data_type, self.getData(), tags=["template"], tlp=self.tlp)]
+    def artifacts(self, raw):
+        artifacts = []
 
-  def run(self):
-    if self.data_type not in ['domain', 'ip']:
-      self.notSupported()
+        artifacts.append(
+          self.build_artifact(self.data_type, self.getData(), tags=["template"], tlp=self.tlp)
+        )
 
-    time.sleep(self.getParam("config.delay", 60))
+        return artifacts
 
-    self.report({'data': self.getData(), 'input': self._input})
+    def run(self):
+        if self.data_type not in ['domain', 'ip']:
+            self.notSupported()
 
-  def summary(self, raw):
-    taxonomies = []
+        time.sleep(self.getParam("config.delay", 60))
 
-    taxonomies.append(
-      self.build_taxonomy('info','Cortex-Analyzer-Template','Records',time.time())
-    )
-    taxonomies.append(
-      self.build_taxonomy('info', 'Cortex-Analyzer-Template', self.data_type, self.getData())
-    )
+        self.report({'data': self.getData(), 'input': self._input})
 
-    return {"taxonomies": taxonomies}
+    def summary(self, raw):
+        taxonomies = []
+
+        taxonomies.append(
+            self.build_taxonomy('info', 'Cortex-Analyzer-Template', 'Records', time.time())
+        )
+        taxonomies.append(
+            self.build_taxonomy('info', 'Cortex-Analyzer-Template', self.data_type, self.getData())
+        )
+
+        return {"taxonomies": taxonomies}
 
 
 if __name__ == '__main__':
-  Template().run()
+    Template().run()
